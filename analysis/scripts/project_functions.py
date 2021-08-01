5619b3611bb71fb270abc78f10bb6121f48198c7
Rhys <rhysherzberg@gmail.com>
2021-08-01 12:51:08 -0700

    Conducted EDA, identified correlations

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,4 +1,3 @@
-
 # first, import the pertinent libraries/packages
 import pandas as pd
 import numpy as np
@@ -7,7 +6,6 @@ import matplotlib.pyplot as plt
 import os
 
 from pandas_profiling import ProfileReport
-
 
 """
 our csv contains aggregate/disaggregate columns. Since we'll be doing our own processing, we'll
@@ -26,16 +24,38 @@ def load_process_data(path_to_file):
     )
     
     drop_list = ["PRUID","Year_Quarter","Time_Period","Value","Aggregator","Unit","Specific Measure","Disaggregator"]
+    event_reassign = {
+        "Total apparent opioid toxicity deaths":1,
+        "Accidental apparent opioid toxicity deaths":2,
+        "Suicide apparent opioid toxicity deaths":3,
+        "Total opioid-related poisoning hospitalizations":4,
+        "Accidental opioid-related poisoning hospitalizations":5,
+        "Intentional opioid-related poisoning hospitalizations":6,
+        "EMS responses to suspected opioid-related overdoses":7,
+        "Total apparent stimulant toxicity deaths":8,
+        "Accidental apparent stimulant toxicity deaths":9,
+        "Accidental stimulant-related poisoning hospitalizations":10,
+        "Total stimulant-related poisoning hospitalizations":11,
+        "Intentional stimulant-related poisoning hospitalizations":12
+    }
+    substance_key = {"Opioids":1,"Stimulants":2}
     
-    # drop columns + add new ones
+    # drop columns, reassign
     df2 = ( df1 
             .drop(drop_list,1)
     )
     
+    df2['Event Category'] = df2['Type of Event'].apply(lambda x: event_reassign[x])
+    df2['Substance'] = df2['Substance'].apply(lambda x: substance_key[x])
     return df2
 
 # initial thoughts: Type of Event is highly correlated with Source and Substance
 
 df2 = load_process_data('../../data/raw/SubstanceHarmsData.csv')
-#print(df2.head(15))
+# checking unique events
+#list_ = pd.unique(df2['Type of Event'])
+#print(list_)
+
+#checking df2 information
+#print(df2.tail(15))
 #df2_profile = ProfileReport(df2).to_notebook_iframe()
